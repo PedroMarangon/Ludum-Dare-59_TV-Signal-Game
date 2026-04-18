6d52651b18c61b94fc9e98d041e0e4de6e911e2c
Pedro Marangon <pedrommarangon@gmail.com>
2026-04-18 19:19:09 -0300

    Adding sprites

--- scripts/interest_channel.gd
+++ scripts/interest_channel.gd
@@ -2,3 +2,9 @@ class_name InterestChannel extends Area2D
 
 
 @export var interest :Interest
+
+
+func _ready() -> void:
+	if interest != null and interest.icon != null:
+		$Sprite2D.texture = interest.icon
+		$Sprite2D.modulate = Color.WHITE
